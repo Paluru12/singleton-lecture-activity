adaf1bb10c90600359af7c9e4cb4293bcf0afed6
Paluru12 <123471667+Paluru12@users.noreply.github.com>
2023-09-25 22:02:31 -0400

    Update Shop.java

--- app/src/main/java/com/example/myapplication/Shop.java
+++ app/src/main/java/com/example/myapplication/Shop.java
@@ -6,7 +6,7 @@ public class Shop {
     private ArrayList<Weapon> weapon_list;
 
     // TODO: Instance should be named "shop"
-
+    private static Shop shop;
     /*
      * TODO: Constructor for Shop
      * Initialize weapon_list to an empty ArrayList
